c2010d37cb34396c1db7ddb8fe964dc0ed0ae801
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-31 22:09:40 +0800

    work in progress.

---
+++ src/src/js/task-06.js
@@ -0,0 +1,11 @@
+function checkNumberCharacters(value) {
+   const maxLength = 6; 
+   document.getElementById('validation-input').value;
+
+
+if (value == maxLength) {
+    document.getElementById("validation-input").style.borderColor = "#4caf50";
+} else {
+    document.getElementById("validation-input").style.borderColor = "#f44336";
+}
+};
