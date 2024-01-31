bcb42df31bdbcd88b1656d2990a045295608d10b
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-31 19:24:43 +0800

    update

---
+++ src/src/js/task-05.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const nameInput = document.getElementById("name-input");
+    const nameOutput = document.getElementById("name-output");
+  
+    nameInput.addEventListener("input", function() {
+      const name = nameInput.value.trim();
+      nameOutput.textContent = name || "Anonymous";
+    });
+  });
