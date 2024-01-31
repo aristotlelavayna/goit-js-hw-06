c0ec0c4bc699daa6165e10ba228f1226e40d38ee
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-31 19:00:55 +0800

    update

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -1,2 +1,7 @@
-console.log(hello world);
-asdasxkcnsd
+let categories = document.getElementsByClassName("item");
+console.log("Number of categories: " + categories.length);
+
+for (let i = 0; i < categories.length; i++) {
+    console.log(`Category: ${categories[i].firstElementChild.innerHTML}`);
+    console.log(`Elements: ${categories[i].firstElementChild.nextElementSibling.children.length}`);
+}
