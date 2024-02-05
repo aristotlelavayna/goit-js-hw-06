f620f0f85d2aa7e740eb2259aeb6c1dacfd7e6b1
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-02-05 18:16:59 +0800

    update

--- src/src/js/task-07.js
+++ src/src/js/task-07.js
@@ -1,10 +1,6 @@
-// original function
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
-
+let input = document.getElementById("font-size-control");
+let text = document.getElementById("text");
+input.addEventListener("input", (e) => {
+    let fs = e.target.value;
+    text.style.fontSize = fs + "px";
+});
